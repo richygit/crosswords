779d33f882da67a388425c0934c8bc7d3882cc6f
rich <richard.yuwono@gmail.com>
2020-04-18 23:18:44 +1000

    added processor to extract crossword matrix and clues

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,20 +1,6 @@
 import React from "react";
 
-export default function FileUploader() {
-  let fileReader;
-
-  const onFileRead = (e) => {
-    const content = fileReader.result;
-    console.log("content: ", content);
-  };
-
-  const onFileChosen = (file) => {
-    console.log("file chosen: ", file);
-    fileReader = new FileReader();
-    fileReader.onloadend = onFileRead;
-    fileReader.readAsText(file);
-  };
-
+export default function FileUploader({ onFileRead, onFileChosen }) {
   return (
     <>
       <h1>Upload your file.</h1>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import FileUploader from "./FileUploader";
+import SmhCrossword from "../lib/SmhCrossword";
 
 export default function Home() {
   //TODO - check if params provided, if so, route to crossword page
 
+  const fileReader = useRef(null);
+
+  const onFileRead = (e) => {
+    const content = fileReader.current.result;
+    let parser = new DOMParser();
+    let dom = parser.parseFromString(content, "text/html");
+
+    const smhCrossword = new SmhCrossword(dom);
+    const matrix = smhCrossword.matrix;
+    console.log("matrix = ", matrix);
+    console.log("across = ", smhCrossword.cluesAcross);
+    console.log("down = ", smhCrossword.cluesDown);
+
+    //reset fileReader since we don't need it any more
+    fileReader.current = null;
+  };
+
+  const onFileChosen = (file) => {
+    console.log("file chosen: ", file);
+
+    fileReader.current = new FileReader();
+    fileReader.current.onloadend = onFileRead;
+    fileReader.current.readAsText(file);
+  };
+
   // otherwise show upload area
-  return <FileUploader />;
+  return <FileUploader onFileChosen={onFileChosen} onFileRead={onFileRead} />;
 }
