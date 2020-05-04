301f8eb97456e1744b813e65fcbf25df300d8ec2
rich <richard.yuwono@gmail.com>
2020-05-04 23:54:35 +1000

    add - encoded data and create shareable url. decode data when url is loaded.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,8 @@ import Crossword from "./Crossword";
 import * as R from "ramda";
 import { SolutionMatrix } from "./Matrix";
 
+const LZString = require("lz-string/libs/lz-string");
+
 const Home: React.FC = () => {
   //TODO - check if params provided, if so, route to crossword page
 
@@ -12,6 +14,43 @@ const Home: React.FC = () => {
   const [matrix, setMatrix] = useState<SolutionMatrix | null>(null);
   const [cluesAcross, setCluesAcross] = useState<ClueGroupData | null>(null);
   const [cluesDown, setCluesDown] = useState<ClueGroupData | null>(null);
+  const [shareUrl, setShareUrl] = useState<string | undefined>(undefined);
+  const [showCopyFlash, setShowCopyFlash] = useState<boolean>(false);
+  const shareRef = useRef<HTMLInputElement>(null);
+
+  const url_string = window.location.href;
+  const url = new URL(url_string);
+  const m = url.searchParams.get("m");
+  const a = url.searchParams.get("a");
+  const d = url.searchParams.get("d");
+
+  useEffect(() => {
+    if (!R.isNil(m) && !R.isNil(a) && !R.isNil(d)) {
+      const decodedMatrix = JSON.parse(
+        LZString.decompressFromEncodedURIComponent(m)
+      );
+      const solutionMatrix = SmhCrossword.solutionMatrixFromData(decodedMatrix);
+      setMatrix(solutionMatrix);
+
+      const decodedAcross = JSON.parse(
+        LZString.decompressFromEncodedURIComponent(a)
+      );
+      setCluesAcross({ ...decodedAcross, ...SmhCrossword.clueGroupData() });
+
+      const decodedDown = JSON.parse(
+        LZString.decompressFromEncodedURIComponent(d)
+      );
+      setCluesDown({ ...decodedDown, ...SmhCrossword.clueGroupData() });
+
+      setShareUrl(
+        `${window.location.href}?${encodeQueryData({
+          m,
+          a,
+          d,
+        })}`
+      );
+    }
+  }, [m, a, d]);
 
   const onFileRead = (e: any) => {
     if (R.isNil(fileReader.current)) {
@@ -23,32 +62,84 @@ const Home: React.FC = () => {
     let dom = parser.parseFromString(String(content), "text/html");
 
     const smhCrossword = new SmhCrossword(dom);
+
     setMatrix(smhCrossword.matrix);
     setCluesAcross(smhCrossword.cluesAcross);
     setCluesDown(smhCrossword.cluesDown);
-    // console.log("matrix = ", smhCrossword.matrix);
-    // console.log("clues = ", [smhCrossword.cluesAcross, smhCrossword.cluesDown]);
+
+    const matrixEncoded = LZString.compressToEncodedURIComponent(
+      JSON.stringify(smhCrossword.compressedMatrix)
+    );
+
+    const acrossEncoded = LZString.compressToEncodedURIComponent(
+      JSON.stringify(smhCrossword.cluesAcross)
+    );
+
+    const downEncoded = LZString.compressToEncodedURIComponent(
+      JSON.stringify(smhCrossword.cluesDown)
+    );
+
+    setShareUrl(
+      `${window.location.href}?${encodeQueryData({
+        m: matrixEncoded,
+        a: acrossEncoded,
+        d: downEncoded,
+      })}`
+    );
 
     //reset fileReader since we don't need it any more
     fileReader.current = null;
   };
 
-  const onFileChosen = (file: any) => {
-    console.log("file chosen: ", file);
+  const encodeQueryData = (data: any) => {
+    const ret = [];
+    for (let d in data)
+      if (data.hasOwnProperty(d)) {
+        ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+      }
+    return ret.join("&");
+  };
 
+  const onFileChosen = (file: any) => {
     fileReader.current = new FileReader();
     fileReader.current.onloadend = onFileRead;
     fileReader.current.readAsText(file);
   };
 
+  const onCopyClick = () => {
+    if (!R.isNil(shareRef.current)) {
+      shareRef.current.select();
+      document.execCommand("copy");
+      setShowCopyFlash(true);
+      window.setTimeout(() => {
+        setShowCopyFlash(false);
+      }, 5000);
+    }
+  };
+
   // otherwise show upload area
   if (!R.isNil(matrix) && !R.isNil(cluesAcross) && !R.isNil(cluesDown)) {
     return (
-      <Crossword
-        matrix={matrix}
-        cluesAcross={cluesAcross}
-        cluesDown={cluesDown}
-      />
+      <>
+        <div>
+          <h2>matrix encoded:</h2>
+          <input
+            ref={shareRef}
+            type="text"
+            disabled={undefined}
+            defaultValue={shareUrl}
+          />
+          <button onClick={onCopyClick}>Copy</button>
+          {showCopyFlash && (
+            <span style={{ background: "#fbff00" }}>Copied!</span>
+          )}
+        </div>
+        <Crossword
+          matrix={matrix}
+          cluesAcross={cluesAcross}
+          cluesDown={cluesDown}
+        />
+      </>
     );
   } else {
     return <FileUploader onFileChosen={onFileChosen} onFileRead={onFileRead} />;
--- src/lib/SmhCrossword.tsx
+++ src/lib/SmhCrossword.tsx
@@ -11,11 +11,16 @@ export interface ClueGroupData {
 
 class SmhCrossword {
   public matrix: SolutionMatrix;
+  //matrix for transmission
+  public compressedMatrix: Array<Array<string | null>>;
   public cluesAcross: any;
   public cluesDown: any;
   constructor(dom: Document) {
-    this.matrix = this.readMatrix(dom);
-    this.setMatrixClues();
+    const [matrix, compressed] = this.readMatrix(dom);
+    this.matrix = matrix;
+    this.compressedMatrix = compressed;
+
+    SmhCrossword.setMatrixClues(this.matrix);
     const [across, down] = this.readClues(dom);
     this.cluesAcross = across; //{ref -> [clue, letter count]}
     this.cluesDown = down;
@@ -23,16 +28,17 @@ class SmhCrossword {
     // window.dom = dom;
   }
 
-  private isActiveCell = (coords: Coords) => {
-    const cell = this.matrix.getCell(coords);
+  private static isActiveCell = (coords: Coords, matrix: SolutionMatrix) => {
+    const cell = matrix.getCell(coords);
     return !R.isNil(cell) && !cell.isBlank;
   };
 
   //returns true if this is a starting cell
-  private checkCell = (
+  private static checkCell = (
     cell: Cell | null,
     coords: Coords,
-    key: number
+    key: number,
+    matrix: SolutionMatrix
   ): boolean => {
     let isStartingCell = false;
 
@@ -46,13 +52,13 @@ class SmhCrossword {
       return isStartingCell;
     }
 
-    if (!this.isActiveCell({ x, y } as Coords)) {
+    if (!SmhCrossword.isActiveCell({ x, y } as Coords, matrix)) {
       return isStartingCell;
     }
 
     // horizontal check - there's no active cell to the left
-    if (!this.isActiveCell({ x: x - 1, y } as Coords)) {
-      if (this.isActiveCell({ x: x + 1, y } as Coords)) {
+    if (!SmhCrossword.isActiveCell({ x: x - 1, y } as Coords, matrix)) {
+      if (SmhCrossword.isActiveCell({ x: x + 1, y } as Coords, matrix)) {
         // if cell has no cell to left but has a cell to the right, then it's a clue start
         cell.isStart = true;
         isStartingCell = true;
@@ -61,15 +67,15 @@ class SmhCrossword {
       }
     } else {
       //there's an active cell to the left - copy the xClueKey
-      const left = this.matrix.getCell({ x: x - 1, y });
+      const left = matrix.getCell({ x: x - 1, y });
       cell.xClueNo = left ? left.xClueNo : null;
     }
 
     // vertical key check
-    if (!this.isActiveCell({ x, y: y - 1 } as Coords)) {
+    if (!SmhCrossword.isActiveCell({ x, y: y - 1 } as Coords, matrix)) {
       //no cell above
 
-      if (this.isActiveCell({ x, y: y + 1 } as Coords)) {
+      if (SmhCrossword.isActiveCell({ x, y: y + 1 } as Coords, matrix)) {
         //if there is also a cell below, then this is a clue start
         cell.isStart = true;
         isStartingCell = true;
@@ -78,7 +84,7 @@ class SmhCrossword {
       }
     } else {
       //there's an active cell above - copy the yClueKey
-      const above = this.matrix.getCell({ x, y: y - 1 });
+      const above = matrix.getCell({ x, y: y - 1 });
       cell.yClueNo = above ? above.yClueNo : null;
     }
 
@@ -86,37 +92,66 @@ class SmhCrossword {
     return isStartingCell;
   };
 
-  private setMatrixClues = () => {
+  public static solutionMatrixFromData = (
+    data: Array<Array<string | undefined>>
+  ): SolutionMatrix => {
+    const cellData: Array<Array<Cell>> = data.map(
+      (row: Array<string | undefined>, y) => {
+        return row.map((txt: string | undefined, x) => {
+          return {
+            answer: txt,
+            isBlank: R.isNil(txt),
+            clueKey: null,
+            isStart: false,
+            xClueNo: null,
+            yClueNo: null,
+            x,
+            y,
+          } as Cell;
+        });
+      }
+    );
+    const solutionMatrix = new SolutionMatrix(cellData);
+    SmhCrossword.setMatrixClues(solutionMatrix);
+    return solutionMatrix;
+  };
+
+  private static setMatrixClues = (matrix: SolutionMatrix) => {
     //set the clue keys based on blanks
     let key = 1;
 
     R.forEach((y) => {
       R.forEach((x) => {
         const coords = { x, y } as Coords;
-        const isStartingCell = this.checkCell(
-          this.matrix.getCell(coords),
+        const isStartingCell = SmhCrossword.checkCell(
+          matrix.getCell(coords),
           coords,
-          key
+          key,
+          matrix
         );
         key = isStartingCell ? key + 1 : key;
-      }, R.range(0, this.matrix.dimX()));
-    }, R.range(0, this.matrix.dimY()));
-
-    console.log(this.matrix);
+      }, R.range(0, matrix.dimX()));
+    }, R.range(0, matrix.dimY()));
   };
 
-  private readMatrix(dom: Document): SolutionMatrix {
+  private readMatrix(
+    dom: Document
+  ): [SolutionMatrix, Array<Array<string | null>>] {
     const data: Array<Array<Cell>> = [];
+    const compressed: Array<Array<string | null>> = [];
+
     const rows = dom.querySelectorAll("#crossword table.printOnly tr");
     Array.from(rows).forEach((tr, y) => {
       const dataRow: Array<Cell> = [];
+      const compressedRow: Array<string | null> = [];
       const cells = tr.querySelectorAll("td");
 
       Array.from(cells).forEach((td, x) => {
         const txt = td.textContent;
+        const isBlank = R.isNil(txt) || txt.trim().length === 0;
         dataRow.push({
           answer: txt,
-          isBlank: !R.isNil(txt) && txt.trim().length === 0,
+          isBlank: isBlank,
           clueKey: null,
           isStart: false,
           xClueNo: null,
@@ -124,21 +159,27 @@ class SmhCrossword {
           x,
           y,
         });
+        compressedRow.push(isBlank ? null : txt);
       });
 
       data.push(dataRow);
+      compressed.push(compressedRow);
     });
 
-    return new SolutionMatrix(data);
+    return [new SolutionMatrix(data), compressed];
   }
 
-  private readClueGroup(group: Element) {
-    const buttons: NodeListOf<Element> = group.querySelectorAll("button");
-    const clues: ClueGroupData = {
+  public static clueGroupData = (): ClueGroupData => {
+    return {
       keys: function () {
         return R.without(["keys"], Object.keys(this)); //don't include this function in the list
       },
     };
+  };
+
+  private readClueGroup(group: Element) {
+    const buttons: NodeListOf<Element> = group.querySelectorAll("button");
+    const clues: ClueGroupData = SmhCrossword.clueGroupData();
 
     Array.from(buttons).forEach((btn) => {
       const ref: string = btn.getAttribute("data-position")!;
